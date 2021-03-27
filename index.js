4bd28695c4104bc4057c50f0183567557c62fbf7
noah the goodra <peterpan0413@live.com>
2021-03-27 15:34:18 -0500

    convert functions of opencities converter

--- index.js
+++ index.js
@@ -16,3 +16,27 @@ ngapp.run(function (exampleService, settingsService) {
         }
     });
 });
+//function GetRecordByCardinal(iFormId: cardinal): IInterface;
+//begin
+//Result := RecordByFormID(FileByLoadOrder(iFormId shr 24), iFormId, true);
+//end;
+function GetRecordByCardinal(iFormId) {
+     xelib.GetRecord(xelib.FileByLoadOrder(iFormId >> 24), iFormId)
+}
+// function GetGridCellString(iElement: IInterface): string;
+// var
+//     grid: TwbGridCell;
+// begin
+// Result := '';
+// if Signature(iElement) <> 'CELL' then
+// exit;
+//
+// grid := GetGridCell(iElement);
+//
+// Result := IntToStr(grid.x) + ',' + IntToStr(grid.y);
+//
+// if Result <> '0,0' then exit; //Skip below if garenteed not persistent
+//
+// if GetIsPersistent(iElement) then //Kind of time consuming check
+// Result := 'Persistent';
+// end;
