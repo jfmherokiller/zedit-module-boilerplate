14674403fadea02a433b2c7328c3cf79f7ae9261
noah the goodra <peterpan0413@live.com>
2021-06-18 15:31:46 -0500

    setup more gulp stuff

--- gulpfile.ts
+++ gulpfile.ts
@@ -7,9 +7,69 @@ import {ParsedPath} from 'gulp-rename';
 import * as zip from 'gulp-zip';
 import * as ts from 'gulp-typescript';
 import * as gfile from 'gulp-file'
+import conventionalRecommendedBump from "conventional-recommended-bump";
+import conventionalGithubReleaser from "conventional-github-releaser";
+import * as execa from "execa";
+import * as dotenv from "dotenv";
 import replace = require("gulp-replace");
+import {promisify} from "util";
+import * as fsprom from 'fs/promises';
 
-function getFormattedDate(date:Date) {
+// load environment variables
+const result = dotenv.config();
+
+if (result.error) {
+    throw result.error;
+}
+// Conventional Changelog preset
+const preset = 'angular';
+// print output of commands into the terminal
+const stdio = 'inherit';
+
+async function bumpVersion() {
+    // get recommended version bump based on commits
+    const {releaseType} = await promisify(conventionalRecommendedBump)({preset});
+    // bump version without committing and tagging
+    await execa('npm', ['version', releaseType, '--no-git-tag-version'], {
+        stdio,
+    });
+}
+
+async function changelog() {
+    await execa(
+        'npx',
+        [
+            'conventional-changelog',
+            '--preset',
+            preset,
+            '--infile',
+            'CHANGELOG.md',
+            '--same-file',
+        ],
+        {stdio}
+    );
+}
+
+async function commitTagPush() {
+    // even though we could get away with "require" in this case, we're taking the safe route
+    // because "require" caches the value, so if we happen to use "require" again somewhere else
+    // we wouldn't get the current value, but the value of the last time we called "require"
+    const {version} = JSON.parse(await fsprom.readFile('package.json', {encoding: "utf8"}));
+    const commitMsg = `chore: release ${version}`;
+    await execa('git', ['add', '.'], {stdio});
+    await execa('git', ['commit', '--message', commitMsg], {stdio});
+    await execa('git', ['tag', `v${version}`], {stdio});
+    await execa('git', ['push', '--follow-tags'], {stdio});
+}
+function githubRelease(done) {
+    conventionalGithubReleaser(
+        { type: 'oauth', token: process.env.GH_TOKEN },
+        { preset },
+        done
+    );
+}
+
+function getFormattedDate(date: Date) {
     let year = date.getFullYear();
     let month = (1 + date.getMonth()).toString().padStart(2, '0');
     let day = date.getDate().toString().padStart(2, '0');
@@ -17,21 +77,25 @@ function getFormattedDate(date:Date) {
     return `${month}/${day}/${year}`;
 }
 
-gulp.task('clean', function() {
-    return gulp.src(['dist','InsectPatch'], {read: false,allowEmpty:true})
+gulp.task('clean', CleanFunction);
+
+function CleanFunction() {
+    return gulp.src(['dist', 'InsectPatch'], {read: false, allowEmpty: true})
         .pipe(clean());
-});
+}
 
 gulp.task('build', gulp.series('clean', ApplyChanges));
+
 function FinalizeInsectRemovalMod() {
     let InsectRTSettings = {
         module: "es2020",
         target: "es2020",
         sourceMap: true,
         moduleResolution: "Node",
-        allowSyntheticDefaultImports: true};
+        allowSyntheticDefaultImports: true
+    };
     let InsectJs = gulp.src("src/InsectPatch/InsectRemoval.ts").pipe(ts(InsectRTSettings))
-        .pipe(replace("export {};",""))
+        .pipe(replace("export {};", ""))
         .pipe(gulp.dest("dist/InsectPatch/"))
     let Modulecode = JSON.stringify({
         "id": "InsectReplacement",
@@ -43,11 +107,13 @@ function FinalizeInsectRemovalMod() {
         "description": "Insect Replacer",
         "moduleLoader": "UPF"
     });
-    let ModulePart = gfile("module.json",Modulecode,{src:true}).pipe(gulp.dest("dist/InsectPatch/"))
-    return [InsectJs,ModulePart]
+    let ModulePart = gfile("module.json", Modulecode, {src: true}).pipe(gulp.dest("dist/InsectPatch/"))
+    return [InsectJs, ModulePart]
 }
+
 function ApplyChanges() {
-    let values = [
+    let values = []
+    let values2 = [
 
         gulp.src('index.js')
             .pipe(include())
@@ -68,27 +134,30 @@ function ApplyChanges() {
         gulp.src('README.md')
             .pipe(gulp.dest('dist'))
     ];
+
     values.push(FinalizeInsectRemovalMod())
     return Promise.all(values);
 }
 
-gulp.task('release', function() {
-    let moduleInfo = JSON.parse(fs.readFileSync('module.json',{encoding:"utf8"})),
+gulp.task('release', ReleaseTask);
+
+function ReleaseTask() {
+    let moduleInfo = JSON.parse(fs.readFileSync('module.json', {encoding: "utf8"})),
         moduleId = moduleInfo.id,
         moduleVersion = moduleInfo.version,
         zipFileName = `${moduleId}-v${moduleVersion}.zip`;
 
     console.log(`Packaging ${zipFileName}`);
 
-    return gulp.src('dist/**/*', { base: 'dist/'})
+    return gulp.src('dist/**/*', {base: 'dist/'})
         .pipe(rename(ParsePathing))
         .pipe(zip(zipFileName))
         .pipe(gulp.dest('.'));
 
-    function ParsePathing(path:ParsedPath) {
-         path.dirname = `${moduleId}/${path.dirname}`;
-         return path
+    function ParsePathing(path: ParsedPath) {
+        path.dirname = `${moduleId}/${path.dirname}`;
+        return path
     }
-});
+}
 
-gulp.task('default', gulp.series('build', 'release'));
+exports.default = gulp.series('build','release')
